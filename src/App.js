4889787acee4a37169fa278c4c5ad5f2f0e43b7d
Abhishek Upadhyay <abhiupadhyay88@gmail.com>
2020-06-17 20:55:24 +0530

    App committed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import BaseRouter from './routes';
 import 'antd/dist/antd.css';
 import CustomLayout from './containers/Layout';
-import FacilityFormView from './containers/FacilityForm';
 
-function App() {
-  return (
-    <div className="App">
-      <CustomLayout>
-        <FacilityFormView />
-      </CustomLayout>
-    </div>
-  );
+
+class App extends Component {  
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <CustomLayout>
+            <BaseRouter />
+          </CustomLayout>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
